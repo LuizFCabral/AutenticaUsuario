35bed1ae168c9ddf80604d5c5fa06f9cc81ada93
Luiz Cabral <luizfelipe.cabralsouza@gmail.com>
2024-03-22 08:46:11 -0300

    Validação de campo. Aveitar somente letras

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 import json
+import re
 
 from model.usuario import Usuario
 
+#verifica se o usuário está cadastrado, somente pelo email ou por email e senha
 def verificar_usuario(objUser, tipo):
     # se caso desejo fazer só a verificação do email
     if (tipo == "cad"):
@@ -21,6 +23,16 @@ def verificar_usuario(objUser, tipo):
                     return True
         return False
 
+
+def validar_campo(campo):
+    # padrão que verifica se o campo contém apenas letras e espaços
+    padrao = r'^[a-zA-Z\s]+$'
+
+    # Verificar se o campo corresponde ao padrão
+    if re.match(padrao, campo):
+        return True
+    else:
+        return False
 
 app = Flask(__name__)
 
@@ -47,21 +59,24 @@ def cadastrar():
     objUser = Usuario(request.form['nome'], request.form['email'], request.form['senha'])
 
     #verifica se o usuário já está cadastrado
-    if verificar_usuario(objUser, "cad"):
-        return render_template('cadastrar.html', mensagem_erro='Este e-mail já está cadastrado.')
+    if validar_campo(objUser.nome):
+        if verificar_usuario(objUser, "cad"):
+            return render_template('cadastrar.html', mensagem_erro='Este e-mail já está cadastrado.')
+        else:
+            #caso o usuário não esteja cadastrado
+            with open('usuarios.json', 'a') as arquivo:
+                usuario = {
+                    "nome": objUser.nome,
+                    "email": objUser.email,
+                    "senha": objUser.criptografar(objUser.senha)
+
+                }
+                json.dump(usuario, arquivo)
+                arquivo.write('\n')
+
+            return render_template('login.html')
     else:
-        #caso o usuário não esteja cadastrado
-        with open('usuarios.json', 'a') as arquivo:
-            usuario = {
-                "nome": objUser.nome,
-                "email": objUser.email,
-                "senha": objUser.criptografar(objUser.senha)
-
-            }
-            json.dump(usuario, arquivo)
-            arquivo.write('\n')
-
-        return render_template('login.html')
+        return render_template('cadastrar.html', mensagem_erro="O nome deve conter apenas letras.")
 
 
 #roda automaticamente o site após uma alteração
